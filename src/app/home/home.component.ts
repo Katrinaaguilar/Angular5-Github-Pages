dd60f3028f6fb9a173bce6af73af546fc19cf00a
katrina aguilar <katrinadawna15@gmail.com>
2018-02-16 19:16:25 +0800

    initial edit for app.module, home components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core
 
 @Component({
   selector: 'app-home',
@@ -7,12 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  itemCount: number = 4;
+  itemCount: number;
   btnText: string = 'Add an item';
+  goalText: String = 'My first life goal';
+  goals = [];
 
   constructor() { }
 
   ngOnInit() {
+   this.itemCount = this.goals.length;
+  }
+
+  addItem() {
+   this.goals.push(this.goalText);
+   this.goalText = '';
+   this.itemCount = this.goals.length;
   }
 
 }
